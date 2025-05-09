bbc72266564a9136eea873823f0a299b572db50f
myselfabhi <abhinavverma2024@gmail.com>
2025-05-09 11:42:45 +0530

    feat: isor button added to Navbar

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -9,6 +9,11 @@ function Navigation() {
         </a>
       </li>
       <li className="nav-li">
+        <a className="nav-link" href="/src/sections/CosmicJourney.jsx">
+          ISRO
+        </a>
+      </li>
+      <li className="nav-li">
         <a className="nav-link" href="#mission">
         Missions
         </a>
